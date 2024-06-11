373b0eb27339bc229f25f6e6d518ab47eae15a8b
KihoonKwon <inyourcareallen@gmail.com>
2024-06-11 18:48:49 +0900

    styled-components basic

--- components/screens/Movies.js
+++ components/screens/Movies.js
@@ -1,34 +1,51 @@
 import React from "react";
-import { TouchableOpacity, Text, View, StyleSheet, Pressable } from "react-native"
+import {
+  TouchableOpacity,
+  Text,
+  View,
+  StyleSheet,
+  Pressable,
+} from "react-native";
+import styled from "styled-components/native";
+
+const Btn = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Title = styled.Text`
+  color: ${(props) => (props.selected ? "blue" : "red")};
+`;
 
 const Movies = ({ navigation: { navigate } }) => (
-    // Tap Stack Native Navigation Integration
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <TouchableOpacity
-            // onPress={() => navigate("Stack", { screen: "Three" })}
-            style={styles.btn}
-        >
-            {/* <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}> */}
-            <Text style={styles.text}>Movies1</Text>
-            {/* </View> */}
-        </TouchableOpacity>
-        <Pressable
-            // onPress={() => navigate("Stack", { screen: "Three" })}
-            style={({ pressed }) => [{ backgroundColor: pressed ? 'rgb(210, 230, 255)' : null }, styles.btn]}
-        >
-            {/* <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}> */}
-            <Text style={styles.text}>Movies2</Text>
-            {/* </View> */}
-        </Pressable>
-    </View>
-)
+  // Tap Stack Native Navigation Integration
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Btn
+    // onPress={() => navigate("Stack", { screen: "Three" })}
+    >
+      <Title selected={false}>Movies1</Title>
+      <Title selected={true}>Movies2</Title>
+    </Btn>
+    {/* <Pressable
+      onPress={() => navigate("Stack", { screen: "Three" })}
+      style={({ pressed }) => [
+        { backgroundColor: pressed ? "rgb(210, 230, 255)" : null },
+        styles.btn,
+      ]}
+    >
+      <Text style={styles.text}>Movies</Text>
+    </Pressable> */}
+  </View>
+);
 
 const styles = StyleSheet.create({
-    btn: {
-        flex: 1, justifyContent: "center", alignItems: "center"
-    },
-    text: {
-        color: "blue"
-    }
-})
-export default Movies
+  btn: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "blue",
+  },
+});
+export default Movies;
