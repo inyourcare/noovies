781592c928f32636c4b48d78d67f5c742d9054ad
KihoonKwon <inyourcareallen@gmail.com>
2024-06-11 18:30:28 +0900

    pressable 사용

--- components/screens/Movies.js
+++ components/screens/Movies.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { TouchableOpacity, Text, View, StyleSheet } from "react-native"
+import { TouchableOpacity, Text, View, StyleSheet, Pressable } from "react-native"
 
 const Movies = ({ navigation: { navigate } }) => (
     // Tap Stack Native Navigation Integration
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <TouchableOpacity
-            onPress={() => navigate("Stack", { screen: "Three" })}
+            // onPress={() => navigate("Stack", { screen: "Three" })}
             style={styles.btn}
         >
             {/* <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}> */}
-            <Text style={styles.text}>Movies</Text>
+            <Text style={styles.text}>Movies1</Text>
             {/* </View> */}
         </TouchableOpacity>
+        <Pressable
+            // onPress={() => navigate("Stack", { screen: "Three" })}
+            style={({ pressed }) => [{ backgroundColor: pressed ? 'rgb(210, 230, 255)' : null }, styles.btn]}
+        >
+            {/* <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}> */}
+            <Text style={styles.text}>Movies2</Text>
+            {/* </View> */}
+        </Pressable>
     </View>
 )
 
