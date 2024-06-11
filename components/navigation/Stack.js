df91f6f18dfee9eb6cc33e414236833488be3a39
KihoonKwon <inyourcareallen@gmail.com>
2024-06-11 15:55:28 +0900

    native navigation 사용

--- components/navigation/Stack.js
+++ components/navigation/Stack.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const ScreenOne = () => <View><Text>One</Text></View>
-const ScreenTwo = () => <View><Text>Two</Text></View>
-const ScreenThree = () => <View><Text>Three</Text></View>
+const ScreenOne = ({ navigation: { navigate } }) => (
+    <TouchableOpacity onPress={() => navigate("Two")}>
+        <Text>One</Text>
+    </TouchableOpacity>)
+const ScreenTwo = ({ navigation: { navigate } }) => (
+    <TouchableOpacity onPress={() => navigate("Three")}>
+        <Text>Two</Text>
+    </TouchableOpacity>)
+const ScreenThree = ({ navigation: { navigate, goBack, setOptions } }) => (
+    <>
+        <TouchableOpacity onPress={() => navigate("One")}>
+            {/* <TouchableOpacity onPress={() => goBack()}> */}
+            <Text>Three</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setOptions({ title: 'Hello' })}>
+            <Text>ChangeTitle</Text>
+        </TouchableOpacity>
+    </>)
 
 const NativeStack = createNativeStackNavigator();
 const Stack = () => <NativeStack.Navigator>
