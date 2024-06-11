fe01605abfbfe5b89043601d72f99be76c41c6e0
KihoonKwon <inyourcareallen@gmail.com>
2024-06-11 11:46:23 +0900

    expo apploading -> splashscreen 변경

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import AppLoading from 'expo-app-loading';
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 // import * as Font from "expo-font"
 import { Ionicons } from "@expo/vector-icons";
 import { NavigationContainer } from '@react-navigation/native';
-import { useAssets } from 'expo-asset';
-import { useFonts } from "expo-font";
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import Tabs from './components/navigation/Tabs';
 
 // const loadFonts = (fonts) => fonts.map(font => Font.loadAsync(font))
@@ -16,33 +15,79 @@ import Tabs from './components/navigation/Tabs';
 
 export default function App() {
   // const [ready, setReady] = useState(false)
-  const [loaded, error] = useFonts(Ionicons.font)
-  const [assets] = useAssets([require('./assets/testimage0001.webp')])
+  // const [loaded, error] = useFonts(Ionicons.font)
+  // const [assets] = useAssets([require('./assets/testimage0001.webp')])
   // if (!ready) {
-  if (!assets || !loaded) {
-    return (<AppLoading
-    // startAsync={async () => {
-    //   // await new Promise(resolve => setTimeout(resolve, 10000))
-    //   // await Font.loadAsync(Ionicons.font)
-    //   // await Font.loadAsync({ Montserrat: require('./assets/fonts/Montserrat.ttf') })
-    //   // await Font.loadAsync({ 'Montserrat-SemiBold': { uri: require('./assets/fonts/Montserrat.ttf'), display: Font.FontDisplay.FALLBACK } })
+  // if (!assets || !loaded) {
+  //   return (<AppLoading
+  //   // startAsync={async () => {
+  //   //   // await new Promise(resolve => setTimeout(resolve, 10000))
+  //   //   // await Font.loadAsync(Ionicons.font)
+  //   //   // await Font.loadAsync({ Montserrat: require('./assets/fonts/Montserrat.ttf') })
+  //   //   // await Font.loadAsync({ 'Montserrat-SemiBold': { uri: require('./assets/fonts/Montserrat.ttf'), display: Font.FontDisplay.FALLBACK } })
 
-    //   // await Asset.loadAsync(require('./assets/testimage0001.webp'))
-    //   // await Image.prefetch('https://hc.snsmd.kr/static/media/homeBackgroundImg.57076584967a8e9cf9c9.png')
+  //   //   // await Asset.loadAsync(require('./assets/testimage0001.webp'))
+  //   //   // await Image.prefetch('https://hc.snsmd.kr/static/media/homeBackgroundImg.57076584967a8e9cf9c9.png')
 
-    //   const fonts = loadFonts([Ionicons.font])
-    //   const assets = loadAssets([require('./assets/testimage0001.webp'), 'https://hc.snsmd.kr/static/media/homeBackgroundImg.57076584967a8e9cf9c9.png'])
-    //   // console.log(assets)
-    //   await Promise.all([...fonts, ...assets])
-    // }}
-    // onFinish={() => setReady(true)}
-    // onError={console.error}
-    />)
+  //   //   const fonts = loadFonts([Ionicons.font])
+  //   //   const assets = loadAssets([require('./assets/testimage0001.webp'), 'https://hc.snsmd.kr/static/media/homeBackgroundImg.57076584967a8e9cf9c9.png'])
+  //   //   // console.log(assets)
+  //   //   await Promise.all([...fonts, ...assets])
+  //   // }}
+  //   // onFinish={() => setReady(true)}
+  //   // onError={console.error}
+  //   />)
+  // }
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(Ionicons.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
+
   return (
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Tabs onLayout={onLayoutRootView} />
+      </NavigationContainer>
+      {/* <View
+        style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        onLayout={onLayoutRootView}>
+        <Text>SplashScreen Demo! 👋</Text>
+        <Ionicons name="rocket" size={30} />
+      </View> */}
+    </>
   )
 }
 
--- components/navigation/Tabs.js
+++ components/navigation/Tabs.js
@@ -3,37 +3,42 @@ import React from 'react'
 import Movies from '../screens/Movies';
 import Search from '../screens/Search';
 import Tv from '../screens/Tv';
+import { useColorScheme } from 'react-native'
 
 const Tab = createBottomTabNavigator();
-const Tabs = () => (
-    <Tab.Navigator screenOptions={{
-        tabBarLabelStyle: {
-            // backgroundColor: "tomato"
-        },
-        tabBarLabelPosition: "beside-icon",
-        tabBarActiveTintColor: "red",
-        tabBarInactiveTintColor: "#555",
-        tabBarStyle: {
-            backgroundColor: 'tomato'
-        },
+const Tabs = () => {
+    const colorScheme = useColorScheme();
+    console.log(colorScheme)
+    return (
+        <Tab.Navigator screenOptions={{
+            tabBarLabelStyle: {
+                // backgroundColor: "tomato"
+            },
+            tabBarLabelPosition: "beside-icon",
+            tabBarActiveTintColor: "red",
+            tabBarInactiveTintColor: "#555",
+            tabBarStyle: {
+                backgroundColor: 'tomato'
+            },
 
-        headerTitleStyle: {
-            color: '#369',
-            // headerRight: () => <View><Text>hello</Text></View>
-        }
-    }}>
-        <Tab.Screen name="Movies" component={Movies}
-            options={{
-                tabBarLabelStyle: {
-                    // backgroundColor: "purple"
-                },
-                // tabBarBadge: 5
-                // tabBarBadge: "hello",
-            }}
-        />
-        <Tab.Screen name="Search" component={Search} />
-        <Tab.Screen name="Tv" component={Tv} />
-    </Tab.Navigator>
-)
+            headerTitleStyle: {
+                color: '#369',
+                // headerRight: () => <View><Text>hello</Text></View>
+            }
+        }}>
+            <Tab.Screen name="Movies" component={Movies}
+                options={{
+                    tabBarLabelStyle: {
+                        // backgroundColor: "purple"
+                    },
+                    // tabBarBadge: 5
+                    // tabBarBadge: "hello",
+                }}
+            />
+            <Tab.Screen name="Search" component={Search} />
+            <Tab.Screen name="Tv" component={Tv} />
+        </Tab.Navigator>
+    )
+}
 
 export default Tabs
