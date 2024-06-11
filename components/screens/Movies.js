4cb332de7896431a6cb657baca1592cb4eb48c28
KihoonKwon <inyourcareallen@gmail.com>
2024-06-11 18:00:43 +0900

    touchable 범위 수정

--- components/screens/Movies.js
+++ components/screens/Movies.js
@@ -3,12 +3,14 @@ import { TouchableOpacity, Text, View } from "react-native"
 
 const Movies = ({ navigation: { navigate } }) => (
     // Tap Stack Native Navigation Integration
-    <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        onPress={() => navigate("Stack", { screen: "Three" })}
-    >
-        {/* <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}> */}
-        <Text>Movies</Text>
-        {/* </View> */}
-    </TouchableOpacity>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <TouchableOpacity
+            onPress={() => navigate("Stack", { screen: "Three" })}
+        >
+            {/* <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}> */}
+            <Text>Movies</Text>
+            {/* </View> */}
+        </TouchableOpacity>
+    </View>
 )
 export default Movies
