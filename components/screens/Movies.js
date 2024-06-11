20d389e63e0474813f953e63b6cf81509a328bbd
KihoonKwon <inyourcareallen@gmail.com>
2024-06-11 18:06:46 +0900

    style basic

--- components/screens/Movies.js
+++ components/screens/Movies.js
@@ -1,16 +1,26 @@
 import React from "react";
-import { TouchableOpacity, Text, View } from "react-native"
+import { TouchableOpacity, Text, View, StyleSheet } from "react-native"
 
 const Movies = ({ navigation: { navigate } }) => (
     // Tap Stack Native Navigation Integration
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <TouchableOpacity
             onPress={() => navigate("Stack", { screen: "Three" })}
+            style={styles.btn}
         >
             {/* <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}> */}
-            <Text>Movies</Text>
+            <Text style={styles.text}>Movies</Text>
             {/* </View> */}
         </TouchableOpacity>
     </View>
 )
+
+const styles = StyleSheet.create({
+    btn: {
+        flex: 1, justifyContent: "center", alignItems: "center"
+    },
+    text: {
+        color: "blue"
+    }
+})
 export default Movies
