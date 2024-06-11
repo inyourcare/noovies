f4c7649a8d7332a03bfef39433b137275ef0e2bf
KihoonKwon <inyourcareallen@gmail.com>
2024-06-11 17:48:49 +0900

    presentation:modal

--- components/navigation/Root.js
+++ components/navigation/Root.js
@@ -4,7 +4,7 @@ import Tabs from './Tabs';
 import Stack from './Stack';
 
 const Nav = createNativeStackNavigator();
-const Root = () => <Nav.Navigator screenOptions={{ headerShown: false }}>
+const Root = () => <Nav.Navigator screenOptions={{ headerShown: false, presentation: "modal" }}>
     <Nav.Screen name='Tabs' component={Tabs}></Nav.Screen>
     <Nav.Screen name='Stack' component={Stack}></Nav.Screen>
 </Nav.Navigator>
