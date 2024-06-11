7e2ed9a137349add0e5493db38f5ed59eb928c9d
KihoonKwon <inyourcareallen@gmail.com>
2024-06-11 14:26:08 +0900

    icon

--- components/navigation/Tabs.js
+++ components/navigation/Tabs.js
@@ -1,19 +1,31 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import React from 'react'
 import Movies from '../screens/Movies';
-import Search from '../screens/Search';
 import Tv from '../screens/Tv';
-import { useColorScheme } from 'react-native'
 import colors from '../../colors';
+import { useColorScheme } from 'react-native';
+import Search from '../screens/Search';
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 const Tabs = () => {
     const isDark = useColorScheme() === "dark";
 
     return (
-        <Tab.Navigator screenOptions={{
+        <Tab.Navigator screenOptions={({ route }) => ({
+            // tabBarIcon: ({ focused, color, size }) => {
+            //     let iconName;
+            //     if (route.name === 'Movies') {
+            //         iconName = focused ? 'film' : 'film';
+            //     } else if (route.name === 'Settings') {
+            //         iconName = focused ? 'ios-list-box' : 'ios-list'
+            //     }
+            //     return <Ionicons name={iconName} size={size} color={color} />
+            // },
             tabBarLabelStyle: {
                 // backgroundColor: "tomato"
+                fontSize: 12,
+                fontWeight: "600"
             },
             tabBarLabelPosition: "beside-icon",
             tabBarActiveTintColor: isDark ? colors.YELLOW_COLOR : colors.BLACK_COLOR,
@@ -29,7 +41,7 @@ const Tabs = () => {
             headerStyle: {
                 backgroundColor: isDark ? colors.BLACK_COLOR : colors.WHITE
             }
-        }}>
+        })}>
             <Tab.Screen name="Movies" component={Movies}
                 options={{
                     tabBarLabelStyle: {
@@ -37,10 +49,17 @@ const Tabs = () => {
                     },
                     // tabBarBadge: 5
                     // tabBarBadge: "hello",
+                    tabBarIcon: ({ color, size }) => (<Ionicons name="film" color={color} size={size} />)
                 }}
             />
-            <Tab.Screen name="Search" component={Search} />
-            <Tab.Screen name="Tv" component={Tv} />
+            <Tab.Screen name="Search" component={Search}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => { return <Ionicons name={focused ? 'search' : 'search-outline'} color={color} size={size} /> }
+                }} />
+            <Tab.Screen name="TV" component={Tv}
+                options={{
+                    tabBarIcon: ({ color, size }) => { return <Ionicons name='tv' color={color} size={size} /> }
+                }} />
         </Tab.Navigator>
     )
 }
